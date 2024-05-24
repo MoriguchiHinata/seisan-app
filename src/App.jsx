668683e3e512de032be40c1e659cf1396feb1989
MoriguchiHinata <h.moriguchi.23.5.966@4nonome.com>
2024-05-24 21:43:11 +0900

    router関連解消

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import Top from "./components/Top";
 import { Routes, Route } from "react-router-dom";
-import "./style.css";
 import Project from "./components/Project";
+import styled from "styled-components";
 
 export const App = () => {
   return (
-    <div>
+    <Container>
       <Routes>
         <Route path="/" element={<Top />} />
         <Route path="/project" element={<Project />} />
-        <Route path="*" />
       </Routes>
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  max-width: 400px;
+`;
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter, Route } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <Route exact path="/" element={<App />} />
+      <Routes>
+        <Route exact path="/" element={<App />} />
+      </Routes>
     </HashRouter>
   </React.StrictMode>
 );
