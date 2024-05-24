947a2861fc4e949f73b72cddd8633aabc7e217bb
MoriguchiHinata <h.moriguchi.23.5.966@4nonome.com>
2024-05-24 21:13:29 +0900

    HashRouterを用いてdeploy

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <Route exact path="/" element={<App />} />
     </HashRouter>
   </React.StrictMode>
 );
