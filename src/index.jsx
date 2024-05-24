ff0d5befb81997b5c8b8b1f3f8272c6e3441358f
MoriguchiHinata <h.moriguchi.23.5.966@4nonome.com>
2024-05-24 15:09:56 +0900

    user agent sheet無効化

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Top from "./components/Top";
 import { BrowserRouter } from "react-router-dom";
+import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
